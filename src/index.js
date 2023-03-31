131014d787d6ee7e55cf6cdaab02156687c7eca5
Himanshu Gupta <himanshugupta558@gmail.com>
2023-04-01 02:04:21 +0530

    Testing database by adding random tweet

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
+const Tweet = require('./models/tweet');
+
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
+
+    const tweet = await Tweet.create({
+        content: 'First tweet',
+        userEmail: 'user1@example.com'
+    });
+    console.log(tweet);
 });
