57970cf04a3167b6c635cd2d72cd40885a3ee148
Himanshu Gupta <himanshugupta558@gmail.com>
2023-04-03 12:33:56 +0530

    Posted a tweet with comment

--- src/index.js
+++ src/index.js
@@ -2,16 +2,25 @@ const express = require('express');
 const connect = require('./config/database');
 const app = express();
 
-const Tweet = require('./models/tweet');
+const TweetRepository = require('./repository/tweet-repository');
+const Comment = require('./models/comment');
 
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
 
-    const tweet = await Tweet.create({
-        content: 'First tweet',
-        userEmail: 'user1@example.com'
-    });
+    // const tweet = await Tweet.create({
+    //     content: 'second tweet',
+    //     userEmail: 'user2@example.com'
+    // });
+    // const tweets = await Tweet.find({userEmail: 'user2@example.com'});
+    // console.log(tweets);
+    const tweetRepo = new TweetRepository();
+    const tweet = await tweetRepo.create({content: 'Tweet with comment'});
+    console.log(tweet);
+    const comment = await Comment.create({content: 'New Comment'});
+    tweet.comments.push(comment);
+    await tweet.save();
     console.log(tweet);
 });
