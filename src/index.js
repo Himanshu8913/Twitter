85686360644298239c39a51ef9582cefdfb587b9
Himanshu Gupta <himanshugupta558@gmail.com>
2023-04-12 12:57:45 +0530

    Testing findByName method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const connect = require('./config/database');
+const { HashtagRepository } = require('./repository');
 const app = express();
 
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
+
+    let repo = new HashtagRepository();
+    const response = await repo.findByName(['Excited', 'CSK']);
+    console.log(response);
 });
